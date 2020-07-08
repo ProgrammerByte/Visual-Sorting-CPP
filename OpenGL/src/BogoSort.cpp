ecb03c2eb382b58d29d299d413a4e002acf5984c
ProgrammerByte <48259254+ProgrammerByte@users.noreply.github.com>
2020-07-08 17:19:11 +0100

    Add files via upload

--- OpenGL/src/BogoSort.cpp
+++ OpenGL/src/BogoSort.cpp
@@ -11,6 +11,9 @@ bool BogoSort::cycle() {
 	vector<float> result;
 	for (int i = 0; i < length; i++) {
 		chosenIndex = (rand() * (length - i)) / RAND_MAX;
+		if (chosenIndex == length - i) { //if out of range due to computational error
+			chosenIndex -= 1;
+		}
 		result.push_back(contents[chosenIndex]);
 		contents.erase(contents.begin() + chosenIndex);
 	}
